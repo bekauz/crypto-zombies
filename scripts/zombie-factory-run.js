9662d3f6f3e0e0b6531cd3823cac6ea53f450c67
bekauz <bekauz@protonmail.com>
2022-01-13 20:06:37 +0100

    updating run script; deleting sample script

--- scripts/zombie-factory-run.js
+++ scripts/zombie-factory-run.js
@@ -6,8 +6,8 @@ async function main() {
   
   console.log("Deploying ZombieFactory...");
   const zombieFactory = await zombieFactoryFactory.deploy();
-
   await zombieFactory.deployed();
+
   console.log("ZombieFactory deployed to:", zombieFactory.address);
 
   let zombies = await zombieFactory.getZombies();
@@ -15,6 +15,13 @@ async function main() {
 
   console.log("Creating a zombie...");
   await zombieFactory.createRandomZombie("name");
+
+  try {
+    console.log("Attempting to create another zombie...");
+    await zombieFactory.createRandomZombie("should not work");
+  } catch (e) {
+    console.log("Failed to create... successfully");
+  }
 
   zombies = await zombieFactory.getZombies();
   console.log("Current number of zombies: ", zombies.length);
