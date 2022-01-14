795251e9ae4479e8a78a428e5b33734573b6700e
bekauz <bekauz@protonmail.com>
2022-01-14 09:01:07 +0100

    adding interface for CryptoKitties contract; method to mix existing kitty dna with zombie

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -4,7 +4,26 @@ pragma solidity ^0.8.0;
 import "./ZombieFactory.sol";
 
 
+abstract contract KittyInterface {
+  function getKitty(uint256 _id) external view virtual returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
+
+
 contract ZombieFeeding is ZombieFactory {
+
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    KittyInterface kittyContract = KittyInterface(ckAddress);
 
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
@@ -12,5 +31,11 @@ contract ZombieFeeding is ZombieFactory {
         _targetDna = _targetDna % dnaModulus;
         uint newDna = (myZombie.dna + _targetDna) / 2;
         _createZombie("name", newDna);
+    }
+
+    function feedOnKitty(uint _zombieId, uint _kittyId) public {
+        uint kittyDna;
+        (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+        feedAndMultiply(_zombieId, kittyDna);
     }
 }
