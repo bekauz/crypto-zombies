49c23b630d5f7877b5b3dc3c07b31921461d6025
bekauz <bekauz@protonmail.com>
2022-01-17 09:31:00 +0100

    adding level up fee to prevent spam; withdraw() method to transfer contract funds to owner address

--- contracts/ZombieHelper.sol
+++ contracts/ZombieHelper.sol
@@ -2,11 +2,32 @@ pragma solidity ^0.8.0;
 
 import "./ZombieFeeding.sol";
 
+
 contract ZombieHelper is ZombieFeeding {
+
+    uint levelUpFee = 0.001 ether;
 
     modifier aboveLevel(uint _level, uint _zombieId) {
         require(zombies[_zombieId].level >= _level);
         _;
+    }
+
+    function withdraw() external onlyOwner {
+        // owner() (openZep) returns a uint160 type address, so we need to
+        // cast it to payable() to be able to transfer funds to that address
+        address payable _owner = payable(owner());
+        // address(this).balance retursn total balance stored on the contract
+        _owner.transfer(address(this).balance);
+    }
+
+    function setLevelUpFee(uint _fee) external onlyOwner {
+        // option to change the fee in case gas gets too expensive
+        levelUpFee = _fee;
+    }
+
+    function levelUp(uint _zombieId) external payable {
+        require(msg.value == levelUpFee);
+        zombies[_zombieId].level++;
     }
 
     function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
